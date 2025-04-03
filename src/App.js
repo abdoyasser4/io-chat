1b426c5df0033e5472b5d47a4d3a4ed1a7db13e9
abdoyasser4 <abdoy83214@gmail.com>
2025-04-03 16:24:08 +0200

    added the chat room connection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import io from "socket.io-client";
-
+import {useState} from "react";
 const socket=io.connect("http://localhost:3001/");
 
 
 function App() {
+  const[userName,setUsername]=useState("");
+  const[room,setRoom]=useState("");
+const joinRoom =()=>{
+  if(userName !=="" && room !==""){
+  socket.emit("join_room",room);
+  }
+}
   return (
     <div className="App">
-    
+    <h3>join a chat</h3>
+    <input type="text" placeholder="Abdo..." onChange={(event)=>{setUsername(event.target.value)}}/>
+    <input type="text" placeholder="room_id" onChange={(event)=>{setRoom(event.target.value)}}/>
+    <button onClick={joinRoom} >join this room</button>
     </div>
   );
 }
